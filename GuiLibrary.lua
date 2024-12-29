ee1bf52a0be4d9e2b48b99fb6115c38c8b0fee80
XxlyitemXx <temjaitnt@gmail.com>
2024-12-29 17:26:44 +0700

    Update GuiLibrary.lua

--- GuiLibrary.lua
+++ GuiLibrary.lua
@@ -143,15 +143,25 @@ if shared.VapeExecuted then
 	GuiLibrary["MainGui"] = gui
 
 	local vapeCachedAssets = {}
-	local function vapeGithubRequest(scripturl)
-		if not isfile("vape/"..scripturl) then
-			local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/vapevoidware/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
-			assert(suc, res)
-			assert(res ~= "404: Not Found", res)
-			if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
-			writefile("vape/"..scripturl, res)
+	local function vapeGithubRequest(scripturl, isImportant)
+		print("Attempting to load: " .. scripturl)
+		if isfile(baseDirectory..scripturl) then
+			print("File exists locally")
+			return readfile(baseDirectory..scripturl)
 		end
-		return readfile("vape/"..scripturl)
+		local suc, res
+		local url = "https://raw.githubusercontent.com/XxlyitemXx/katware/main/"
+		print("Requesting from URL:", url..scripturl)
+		suc, res = pcall(function() 
+			return game:HttpGet(url..scripturl, true) 
+		end)
+		if not suc or res == "404: Not Found" then
+			print("Failed to fetch file:", res)
+			if isImportant then
+				warn("Failed to connect to github:", baseDirectory..scripturl, res)
+			end
+		end
+		return res
 	end
 
 	local function downloadVapeAsset(path)
