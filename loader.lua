557ba6b0a7751b7d77a3800a4382640d861eb800
XxlyitemXx <temjaitnt@gmail.com>
2025-01-26 12:54:52 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -11,7 +11,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/XxlyitemXx/katware'..readfile('katware/profiles/commit.txt')..'/'..select(1, path:gsub('katware/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/XxlyitemXx/katware'..readfile('katware/profiles/commit.txt')..'/'..select(1, path:gsub('katware/main.lua', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
